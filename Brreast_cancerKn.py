e534f133af9cab24f562314342250022b1940016
AboodRagnar <126026547+AboodRagnar@users.noreply.github.com>
2023-02-21 21:08:44 -0500

    Add files via upload

---
+++ Brreast_cancerKn.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier 
+
+def switch(data):
+    
+
+    if data==[2]:
+        return 'Benign'
+    
+    if data==[4]:
+        return 'Malignant'
+
+GetRawData=pd.read_csv('T1/breast-cancer-wisconsin.csv',usecols=[1,2,3,4,5,6,7,8,9,10])
+
+Features=GetRawData.drop(columns='Class')
+lable=GetRawData['Class']
+BulidClassifier=KNeighborsClassifier(n_neighbors=1)
+BulidClassifier.fit(X=Features,y=lable)
+
+print(switch(BulidClassifier.predict([[1,1,1,1,5,1,3,1,1]])))
+
+
